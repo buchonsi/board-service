03c71690713c0783520a99d49f0d399a922df1bd
buchonsi <buchonsi@naver.com>
2023-10-06 13:08:32 +0900

    #1 - data rest 설정 게시글, 댓글의 json api 를 자동으로 restful 하게 만들게끔 어노테이션 추가.

--- src/main/java/com/yoon/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/yoon/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.yoon.projectboard.repository;
 
 import com.yoon.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
