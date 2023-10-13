22a56e341763bcf0916ce0a96ad207b37c1e5106
buchonsi <buchonsi@naver.com>
2023-10-13 22:34:05 +0900

    #17 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은 `data.sql` 테스트 데이터도 모두 업데이트.
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용.

--- src/test/java/com/yoon/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/yoon/projectboard/repository/JpaRepositoryTest.java
@@ -2,6 +2,7 @@ package com.yoon.projectboard.repository;
 
 import com.yoon.projectboard.config.JpaConfig;
 import com.yoon.projectboard.domain.Article;
+import com.yoon.projectboard.domain.UserAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +19,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
+    private final UserAccountRepository userAccountRepository;
     private final ArticleCommentRepository articleCommentRepository;
 
     public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
-                             @Autowired ArticleCommentRepository articleCommentRepository) {
+                             @Autowired ArticleCommentRepository articleCommentRepository,
+                             @Autowired UserAccountRepository userAccountRepository) {
         this.articleRepository = articleRepository;
+        this.userAccountRepository = userAccountRepository;
         this.articleCommentRepository = articleCommentRepository;
     }
 
@@ -45,9 +49,11 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine() {
         //given
         long previousCount = articleRepository.count();
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("yoontest", "pw", null, null, null));
+        Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
         //when
-        Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+        Article savedArticle = articleRepository.save(article);
 
         //then
         assertThat(articleRepository.count())
