077ce29f8d7bf12801ce9a1598dbea540d8937a0
buchonsi <buchonsi@naver.com>
2023-11-17 03:10:54 +0900

    #71 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가.
    해당 변경을 테스트에도 반영

--- src/test/java/com/yoon/projectboard/config/TestSecurityConfig.java
+++ src/test/java/com/yoon/projectboard/config/TestSecurityConfig.java
@@ -1,30 +1,37 @@
 package com.yoon.projectboard.config;
 
-import com.yoon.projectboard.domain.UserAccount;
-import com.yoon.projectboard.repository.UserAccountRepository;
+import com.yoon.projectboard.dto.UserAccountDto;
+import com.yoon.projectboard.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
 
     @MockBean
-    private UserAccountRepository userAccountRepository;
+    private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetup() {
         String userId = "yoonSecurity";
-        given(userAccountRepository.findById(userId)).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(userId)).willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "yoonSecurity",
                 "pwSecurity",
                 "yoonSecurity@naver.com",
                 "Yoon-Security",
                 "yoon Security memo"
-        )));
+        );
     }
 }
